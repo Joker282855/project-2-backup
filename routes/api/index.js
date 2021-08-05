1d9e6a7ac3a6c9e79686b0853f1c6de6fc285bc0
unknown <jtjackson325@gmail.com>
2021-08-04 23:30:39 -0600

    got the other routes added into the backend

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,8 +2,12 @@ const router = require('express').Router();
 
 const calendarRoutes = require('./calendar-routes.js');
 const bellRoutes = require('./bell-routes.js');
+const contactRoutes = require('./contact-routes.js');
+const newsfeedRoutes = require('./newsfeed-routes.js');
 
 router.use('/calendars', calendarRoutes);
 router.use('/bells', bellRoutes);
+router.use('/contacts', contactRoutes);
+router.use('/newsfeeds', newsfeedRoutes);
 
 module.exports = router;
