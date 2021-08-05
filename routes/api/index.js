00552610d2c955d3633ba70608d6ad113bb5059e
unknown <jtjackson325@gmail.com>
2021-08-04 23:05:11 -0600

    got the second route page for the bell-routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 
 const calendarRoutes = require('./calendar-routes.js');
+const bellRoutes = require('./bell-routes.js');
 
 router.use('/calendars', calendarRoutes);
+router.use('/bells', bellRoutes);
 
 module.exports = router;
